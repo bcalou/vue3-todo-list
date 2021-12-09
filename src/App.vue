2878bbaac2419de0e6b45a957fad1e80de92eef0
bcalou <bastien.calou@gmail.com>
2021-12-09 19:03:33 +0100

    aset up todo list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Todo List</h1>
+
+  <todo-new @add-new-todo="addNewTodo"></todo-new>
+
+  <article v-for="todo of todos" :key="todo.id">
+    <input type="checkbox" v-model="todo.done" :id="todo.id" />
+    <label :for="todo.id">{{ todo.label }}</label>
+  </article>
+
+  {{ doneCount }} tâches réalisées sur {{ todos.length }} au total.
+
+  <button v-on:click="removeDoneTodos">Retirer les todos effectuées</button>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import TodoNew from "./components/todo-new";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    TodoNew,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          label: "Manger",
+          done: true,
+        },
+        {
+          id: 2,
+          label: "Dormir",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    addNewTodo(newTodoName) {
+      this.todos.push({
+        id: Date.now(),
+        label: newTodoName,
+        done: false,
+      });
+    },
+    removeDoneTodos() {
+      this.todos = this.todos.filter((todo) => !todo.done);
+    },
+  },
+  computed: {
+    doneCount() {
+      return this.todos.filter((todo) => todo.done).length;
+    },
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/todo-new.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit.prevent="addNewTodo">
+    <label for="newTodo">Nouvelle todo</label>
+    <input id="newTodo" required v-model="newTodoName" />
+    <button>Ajouter</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: Array,
+  },
+  data() {
+    return {
+      newTodoName: "",
+    };
+  },
+  methods: {
+    addNewTodo() {
+      this.$emit("addNewTodo", this.newTodoName);
+      this.newTodoName = "";
+    },
+  },
+};
+</script>
